b3622c3ce108f8ea3d57ab65a6234290684e75d6
daxen98 <ljubisavljevic.danijel98@gmail.com>
2022-05-04 20:14:56 +0200

    Added more colors

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var color3 = document.querySelector(".color3");
+var color4 = document.querySelector(".color4");
 var body = document.getElementById("gradient");
 var random = document.querySelector(".random");
 
 function setGradient()
 {
-    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ", " + color3.value + ", " + color4.value + ")";
     css.textContent = body.style.background + ";";
 }
 
@@ -25,6 +27,8 @@ function setRandomColor()
 {
     color1.value = getRandomColor();
     color2.value = getRandomColor();
+    color3.value = getRandomColor();
+    color4.value = getRandomColor();
     setGradient();
 }
 
@@ -32,5 +36,7 @@ window.addEventListener(document.onload, setGradient());
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+color3.addEventListener("input", setGradient);
+color4.addEventListener("input", setGradient);
 
 random.addEventListener("click", setRandomColor);
